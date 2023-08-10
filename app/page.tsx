bc973edbf16fcdae2faf4acb40140e45716e607b
Oladejo Toyyib <oladejotoyyibolalekan@gmail.com>
2023-08-10 10:52:04 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/olatoydev.jpg" />
+      </Head>
+      <main className="">
+        
+      </main>
+    </>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,110 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+// import wordanalyticsImg from "@/public/wordanalytics.png";
+import recipebannerImg from "@/public/new-banner.png";
+import olatoydashboardImg from "@/public/olatoy-dashboard.png"
+
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+  {
+    name: "Blog",
+    hash: "#blog",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Graduated bootcamp",
+    location: "Miami, FL",
+    description:
+      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2019",
+  },
+  {
+    title: "Front-End Developer",
+    location: "Orlando, FL",
+    description:
+      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2019 - 2021",
+  },
+  {
+    title: "Full-Stack Developer",
+    location: "Houston, TX",
+    description:
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+    icon: React.createElement(FaReact),
+    date: "2021 - present",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Food-recipe",
+    description:
+      "this is a food recipe app where the user can search meals and save meals for later. I used React query to fetch, cache & prefetch the data. I used APIs from TheMealDB: which is an open, crowd-sourced database of Recipes from around the world.",
+    tags: ["React", "Next.js", "ReactQuery", "Tailwind", "mealDB APIs"],
+    imageUrl: recipebannerImg,
+  },
+  {
+    title: "Admin Dashboard",
+    description:
+      "this is a react admin dashboard build with a pure reactjs using scss and installing react with vite",
+    tags: ["React", "JavaScript", "scss"],
+    imageUrl: olatoydashboardImg,
+  },
+  {
+    title: "Word Analytics",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    imageUrl: olatoydashboardImg,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "TailwindCSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Cypress",
+  "Jest",
+  "Github",
+  "Bitbucket",
+  "MongoDB",
+  "Prisma",
+  "Netlify",
+  "Redux",
+  "PostgreSQL",
+] as const;
