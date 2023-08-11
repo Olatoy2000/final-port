da3b8ab6800005f546951090558f2f608e87893e
Oladejo Toyyib <oladejotoyyibolalekan@gmail.com>
2023-08-11 16:18:51 +0100

    fix: styling on project secion

--- components/project.tsx
+++ components/project.tsx
@@ -1,42 +1,55 @@
+"use client";
 import { projectsData } from "@/lib/data";
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
-// interface ProjectProps {
-//   title: string;
-//   description: string;
-//   tags: (string | string[])[];
-//   imageUrl: string;
-// }
+import { useScroll, motion, useTransform } from "framer-motion";
 
 type ProjectProps = (typeof projectsData)[number];
 
 const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.33 1"],
+  });
+
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
   return (
-    <section
-      className="group bg-gray-100 relative max-w-[42rem] border border-black/5 overflow-hidden
-    sm:pr-8 sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8"
+    <motion.div
+      ref={ref}
+      style={{
+        scale: scaleProgress,
+        opacity: opacityProgress,
+      }}
+      className="mb-3 sm:mb-8 last:mb-0 group"
     >
-      <div className="pt-4 pb-7 group-even:ml-[18rem] px-5 sm:pl-10 h-full sm:pr-2 sm:pt-10 sm:max-w-[50%] ">
-        <h3 className="text-2xl font-semibold">{title}</h3>
-        <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
-        <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-          {tags.map((tag, index) => (
-            <li
-              key={index}
-              className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <Image
-        src={imageUrl}
-        alt="Project I worked on"
-        quality={95}
-        className="absolute group-even:right-[initial] group-even:-left-40 top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl"
-      />
-    </section>
+      <section
+        className="bg-gray-100 relative max-w-[42rem] border border-black/5 overflow-hidden
+      sm:pr-8 sm:h-[20rem] rounded-lg hover:bg-gray-200 transition group-even:pl-8"
+      >
+        <div className="pt-4 pb-7 group-even:ml-[18rem] px-5 sm:pl-10 h-full sm:pr-2 sm:pt-10 sm:max-w-[50%]">
+          <h3 className="text-2xl font-semibold">{title}</h3>
+          <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+            {tags.map((tag, index) => (
+              <li
+                key={index}
+                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
+              >
+                {tag}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <Image
+          src={imageUrl}
+          alt="Project I worked on"
+          quality={95}
+          className="absolute group-even:right-[initial] group-even:-left-40 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04] group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl"
+        />
+      </section>
+    </motion.div>
   );
 };
 
